5dc9dc9cb79f1415f6309ddb160488ebbcd3df97
Deosrc <deosrc@g-de.co.uk>
2024-09-26 22:18:47 +0000

    Add issues for yaml deprecation

--- homeassistant/components/hikvision/config_flow.py
+++ homeassistant/components/hikvision/config_flow.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import voluptuous as vol
 
-from homeassistant import config_entries
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.const import (
     CONF_HOST,
     CONF_NAME,
@@ -13,7 +13,9 @@ from homeassistant.const import (
     CONF_SSL,
     CONF_USERNAME,
 )
+from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 
 from . import HikvisionData
 from .const import DEFAULT_PORT, DOMAIN
@@ -40,7 +42,35 @@ class HikvisionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, legacy_config: dict
     ) -> config_entries.ConfigFlowResult:
         """Import the legacy yaml config."""
-        return await self.async_step_user(legacy_config)
+        try:
+            conn = await self.hass.async_add_executor_job(
+                self._create_connection, legacy_config
+            )
+            result = await self._async_create_entry(legacy_config, conn)
+            self._create_deprecated_yaml_issue()
+        except data_entry_flow.AbortFlow as err:
+            if err.reason == "already_configured":
+                self._create_deprecated_yaml_issue()
+                return self.async_abort(reason="already_configured")
+            raise
+        except ConnectionError as err:
+            async_create_issue(
+                self.hass,
+                DOMAIN,
+                "deprecated_yaml_cannot_connect",
+                is_fixable=False,
+                is_persistent=False,
+                issue_domain=DOMAIN,
+                severity=IssueSeverity.WARNING,
+                translation_key="deprecated_yaml_import_issue_cannot_connect",
+                translation_placeholders={
+                    "integration_title": "Hikvision",
+                    "url": "https://www.home-assistant.io/integrations/hikvision/",
+                },
+            )
+            raise data_entry_flow.AbortFlow("deprecated_yaml_import_failed") from err
+        else:
+            return result
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -49,35 +79,54 @@ class HikvisionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            host = user_input[CONF_HOST]
-            port = user_input.get(CONF_PORT)
-
-            result = await self.hass.async_add_executor_job(
-                self.create_connection,
-                host,
-                port,
-                user_input.get(CONF_SSL, False),
-                user_input[CONF_USERNAME],
-                user_input[CONF_PASSWORD],
-            )
-
-            if result.cam_id:
-                await self.async_set_unique_id(str(result.cam_id))
-                self._abort_if_unique_id_configured()
-                title = user_input.get(CONF_NAME) or result.name or host
-                return self.async_create_entry(title=title, data=user_input)
-
-            errors["base"] = "connection_failed"
+            try:
+                conn = await self.hass.async_add_executor_job(
+                    self._create_connection, user_input
+                )
+                return await self._async_create_entry(user_input, conn)
+            except ConnectionError:
+                errors["base"] = "connection_failed"
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
         )
 
-    def create_connection(
-        self, host: str, port: int | None, is_https: bool, username: str, password: str
-    ) -> HikvisionData:
-        """Check the connection to a Hikvision camera or NVR.
+    async def _async_create_entry(
+        self, conf: dict, conn: HikvisionData
+    ) -> config_entries.ConfigFlowResult:
+        """Create the config entry."""
+        await self.async_set_unique_id(str(conn.cam_id))
+        self._abort_if_unique_id_configured()
+        title = conf.get(CONF_NAME) or conn.name or conf[CONF_HOST]
+        return self.async_create_entry(title=title, data=conf)
 
-        Returns the name of the Camera/NVR, or None if a connection could not be established.
-        """
-        return HikvisionData(host, port, is_https, None, username, password)
+    def _create_deprecated_yaml_issue(self) -> None:
+        """Create an issue for successful yaml import."""
+        async_create_issue(
+            self.hass,
+            HOMEASSISTANT_DOMAIN,
+            f"deprecated_yaml_{DOMAIN}",
+            is_fixable=False,
+            is_persistent=False,
+            issue_domain=DOMAIN,
+            severity=IssueSeverity.WARNING,
+            translation_key="deprecated_yaml",
+            translation_placeholders={
+                "domain": DOMAIN,
+                "integration_title": "Hikvision",
+            },
+        )
+
+    def _create_connection(self, inputs: dict) -> HikvisionData:
+        """Create a connection to the Hikvision camera or NVR."""
+        host = inputs[CONF_HOST]
+        port = inputs.get(CONF_PORT)
+        is_https = inputs.get(CONF_SSL, False)
+        username = inputs[CONF_USERNAME]
+        password = inputs[CONF_PASSWORD]
+
+        conn = HikvisionData(host, port, is_https, None, username, password)
+        if conn.cam_id:
+            return conn
+
+        raise ConnectionError
